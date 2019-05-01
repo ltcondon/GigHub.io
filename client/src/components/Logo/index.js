36d1a040b7ebd1e71e284f14ab431f5b58807f9d
Thor Nolan <tnolan5764@gmail.com>
2019-05-01 14:35:36 -0700

    Add favicon, update nav component and add new quick way to login from nav

---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// This file exports the logo component for use in the navbar
+
+export default function Logo() {
+  return (
+    <img src="./img/gighub-logo.png" alt="GigHub logo" className="logo-image" />
+  );
+}
