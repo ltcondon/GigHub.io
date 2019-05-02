9857bd1e8d7ccecf81d2a5dbdc40d1c2c6b4b4a0
Liam Condon <condonliamt@gmail.com>
2019-05-01 22:59:56 -0700

    updated controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,25 +1,7 @@
-const db = require("../models");
+const db = require("../backend/models");
 
-// Defining methods for the booksController
-module.exports = {
-  findAll: function(req, res) {
-    db.Book
-      .find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
-    db.Book
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  remove: function(req, res) {
-    db.Book
-      .findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
-};
+module.exports = function(app) {
+	app.post("/api/users", (request, response) => {
+		db.User.create(request.body)
+	})
+}
