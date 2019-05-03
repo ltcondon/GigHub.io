22e22c37a778c2cacb16fb2b469fee88f3ca405e
Liam Condon <condonliamt@gmail.com>
2019-05-02 23:27:20 -0700

    restructured directory, cleaned out servers, got routes working via proxy server, updated package jsons; the bin wwww needs sorted

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+const routes = require("../routes");
 var callback = require('./routes/callback');
 
 var app = express();
@@ -13,8 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'client/src/views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(routes);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
