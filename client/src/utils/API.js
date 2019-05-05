16236375ce46c20505d68a09da181a61ef7d412f
bavarianstance <bavarianstance@gmail.com>
2019-05-04 18:59:08 -0700

    fixed cors errorg

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
 
   searchGlassdoor: function(search) {
-    return axios.get(`https://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=177283&t.k=ffvMT8m8Fem&action=employers&userip=192.168.43.42&useragent=Mozilla/%2F4.0&callback=?${search}`)
+    return axios.get(`https://cors-anywhere.herokuapp.com/https://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=177283&t.k=ffvMT8m8Fem&action=employers&userip=192.168.43.42&useragent=Mozilla/%2F4.0&callback=?&q=${search}`)
   },
 
   saveJob: function (jobData) {
