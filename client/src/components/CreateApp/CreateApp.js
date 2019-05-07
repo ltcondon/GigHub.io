a33bd1fe4816ac1155c13d097e14aa353c58af36
Liam Condon <condonliamt@gmail.com>
2019-05-06 21:06:42 -0700

    add job form now saves in mongoDB

--- client/src/components/CreateApp/CreateApp.js
+++ client/src/components/CreateApp/CreateApp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-// import API from '../../utils/API'
+import API from '../../utils/API'
 // import { Redirect } from 'react-router-dom';
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
@@ -13,7 +13,12 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 
 export default class FormDialog extends Component {
     state = {
-      open: true
+      open: true,
+      company: '',
+      role: '',
+      status: '',
+      milestone: '',
+      createdAt: ''
     };
 
     componentDidMount () {
@@ -34,19 +39,69 @@ export default class FormDialog extends Component {
 
     }
 
+    onChangeCompany = (e) => {
+        this.setState({
+            company: e.target.value
+        });
+    }
+
+    onChangeRole = (e) => {
+        this.setState({
+            role: e.target.value
+        });
+    }
+
+    onChangeStatus = (e) => {
+        this.setState({
+            status: e.target.value
+        });
+    }
+
+    onChangeMilestone = (e) => {
+        this.setState({
+            milestone: e.target.value
+        });
+    }
+
+    publish = (e) => {
+        e.preventDefault();
+
+        console.log(`Job App Created`);
+        console.log(`Company: ${this.state.company}`);
+        console.log(`Role: ${this.state.role}`);
+        console.log(`Status: ${this.state.status}`);
+        console.log(`Milestone: ${this.state.milestone}`);
+        console.log(`Date: ${new Date()}`);
+        console.log(`UserID: ${this.state.id}`);
+
+        const newJob = {
+            company: this.state.company,
+            role: this.state.role,
+            status: this.state.status,
+            milestone: this.state.milestone,
+            userID: this.state.id
+        };
+
+        // console.log(newJob);
+
+        API.saveJob(newJob)
+        .then(res => {
+            console.log(res.status, res.statusText);
+            alert('Job Added!', {type: 'success'})
+            API.getUserJobs(this.state.id)
+            .then(userJobs => {
+                userJobs.data.map(userJob => {
+                    return console.log(userJob);
+
+                })
+                window.location.reload()
+            })
+        })
+    }
+
     render() {
         if (!this.state.open) {
-            // return <Redirect to={{
-            //   pathname: '/dashboard/myjobs', 
-            //   state: {
-            //     id: this.state.id,
-            //     firstName: this.state.firstName,
-            //     lastName: this.state.lastName,
-            //     pictureURL: this.state.pictureURL,
-            //     isAuthorized: true
-            //   }
-            // }}  
-            // />
+
             window.location.reload()
         }
     
@@ -61,7 +116,7 @@ export default class FormDialog extends Component {
           >
             <DialogTitle id="form-dialog-title">Add Job Details</DialogTitle>
             <DialogContent>
-              <DialogContentText>Congrats <span className="userName">{this.state.firstName} {this.state.lastName}!</span> Way to take initiative! The first step can often be the hardest, but we're here to help. Provide some details below.</DialogContentText>
+              <DialogContentText>Congrats <span className="userName">{this.state.firstName}!</span> Way to take initiative! The first step can often be the hardest, but we're here to help. Provide some details below.</DialogContentText>
               <TextField
                 autoFocus
                 margin="dense"
@@ -69,6 +124,7 @@ export default class FormDialog extends Component {
                 label="Company"
                 type="text"
                 placeholder="Facebook"
+                onChange={this.onChangeCompany}
                 fullWidth
               />
             </DialogContent>
@@ -81,6 +137,7 @@ export default class FormDialog extends Component {
                 label="Role"
                 type="text"
                 placeholder="Software Engineer"
+                onChange={this.onChangeRole}
                 fullWidth
               />
             </DialogContent>
@@ -93,6 +150,7 @@ export default class FormDialog extends Component {
                 label="Status"
                 type="text"
                 placeholder="In Progress"
+                onChange={this.onChangeStatus}
                 fullWidth
               />
             </DialogContent>
@@ -105,6 +163,7 @@ export default class FormDialog extends Component {
                 label="Milestone"
                 type="text"
                 placeholder="Applied"
+                onChange={this.onChangeMilestone}                
                 fullWidth
               />
             </DialogContent>
@@ -112,7 +171,7 @@ export default class FormDialog extends Component {
               <Button onClick={this.handleClose} color="primary">
                 Cancel
               </Button>
-              <Button onClick={this.handleClose} color="primary">
+              <Button onClick={this.publish} color="primary">
                 Apply
               </Button>
             </DialogActions>
