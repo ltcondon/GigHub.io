6a2d0faf5e59f57663f30024589b6756bcee96a8
Thor Nolan <tnolan5764@gmail.com>
2019-06-03 13:40:04 -0700

    Fix lifecycle methods on contact component so that contacts are shown as soon as user navigates to contacts

--- client/src/components/Contacts/index.js
+++ client/src/components/Contacts/index.js
@@ -33,12 +33,16 @@ class Contacts extends Component {
   // Grab state passed down from contact route from dashboard
   componentDidMount () {
     this.setState({...this.props.state});
+    this.getContacts();
   };
 
-  // Grab user contacts from db when component updates
-  // componentDidUpdate () {
-  //   this.getContacts();
-  // };
+  // Grab user contacts from db when component updates if userID has been passed down to this components' state
+  componentDidUpdate (prevProps) {
+    if (this.props.state.id !== prevProps.state.id) {
+      this.setState({...this.props.state});
+      this.getContacts();
+    }
+  };
   
   // Event handlers for opening and closing modal dialog
   handleClickOpen = () => {
@@ -88,7 +92,7 @@ class Contacts extends Component {
 
   // Hit the get API route for all contacts associated with current user's unique ID, and update network number to reflect number of contacts
   getContacts = () => {
-    API.getUserContacts(this.state.id)
+    API.getUserContacts(this.props.state.id)
         .then(res => {
           // console.log(res);
           this.setState({apiContacts : res.data});
